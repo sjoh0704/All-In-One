ede6fc19b47187114e104b45abd1c3dd855924c9
seung <sjoh0704@daum.net>
2022-05-14 20:46:21 +0900

    update

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -15,7 +15,7 @@ resource "aws_security_group_rule" "eks-cluster-egress" {
   type = "egress" 
   cidr_blocks = ["0.0.0.0/0"]  # to
   to_port = 65535
-  from_port = 1025
+  from_port = 0
   protocol = "-1"
   security_group_id = aws_security_group.eks-cluster.id 
 }
